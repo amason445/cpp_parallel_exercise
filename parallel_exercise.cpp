6d98b02c952a21acd8e1a05da9493482696f6117
amason445 <masona632@gmail.com>
2022-03-11 06:11:51 -0700

    Adding Source and Readme

---
+++ parallel_exercise.cpp
@@ -0,0 +1,68 @@
+#include <future>
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <iostream>
+
+void print_vector(std::vector<int> vec) {
+ for (auto i: vec) {
+  std::cout << i << ' ';
+  }	 
+} 
+
+std::vector<int> sort_vector(std::vector<int> vec) {
+
+std::sort(vec.begin(), vec.end());
+return vec;
+
+}
+
+void promise_vector(std::promise<std::vector<int>>&& vecPromise, std::vector<int> vec) {
+ 
+ std::vector<int> result = sort_vector(vec); 
+ vecPromise.set_value(result);	
+
+}
+
+int main() {
+ 
+ //define input vectors	
+ std::vector<int> v1 = {1,57,13,16,19,20};
+ std::vector<int> v2 = {8,9,12,124,86,91};
+
+ //post inputs
+ std::cout << "The inputs are: " << std::endl;
+ print_vector(v1);
+ std::cout << std::endl;
+ print_vector(v2);
+ std::cout << std::endl;
+
+ //define promises
+ std::promise<std::vector<int>> v1Promise;
+ std::promise<std::vector<int>> v2Promise;
+
+ // get the futures
+ std::future<std::vector<int>> v1Future = v1Promise.get_future();
+ std::future<std::vector<int>> v2Future = v2Promise.get_future();
+
+ std::thread t1 {promise_vector, std::move(v1Promise), v1};
+ std::thread t2 {promise_vector, std::move(v2Promise), v2};
+
+ std::vector<int> res1 = v1Future.get(); 
+ std::vector<int> res2 = v2Future.get();
+
+ t1.join();
+ t2.join();
+
+ std::cout << "The outputs are: " << std::endl;
+ print_vector(res1);
+ std::cout << std::endl;
+ print_vector(res2);
+ std::cout << std::endl;
+
+}
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Parallel Processing Exercise
+##A brief Exercise in Parallel Processing
+
+This code takes two different vectors and sorts them in two different threads, returning the sorted values.
